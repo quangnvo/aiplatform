38d0c0b64d75a78bdb5c2a196a102bf7e8a40a44
quangnvo <voquang.usth@gmail.com>
2023-10-31 15:26:58 +0800

    setup folder

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardPage = () => {
+    return (
+        <div>DashboardPage (Unprotected)</div>
+    )
+}
+
+export default DashboardPage
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      Landing Page (unprotected)
+    </div>
+  )
+}
