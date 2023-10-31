68e84743d97b6c528d9359167118fad9e42a5c32
quangnvo <voquang.usth@gmail.com>
2023-10-31 18:34:28 +0800

    add clerk authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const DashboardPage = () => {
-    return (
-        <div>DashboardPage (Unprotected)</div>
-    )
+	return (
+		<div>
+			<p>DashboardPage (Protected)</p>
+			{/* <UserButton afterSignOutUrl='/' /> */}
+		</div>
+	)
 }
 
 export default DashboardPage
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,26 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       Landing Page (unprotected)
+      <div>
+
+        {/* Button sign in */}
+        <Link href="/sign-in">
+          <Button>
+            Login
+          </Button>
+        </Link>
+
+        {/* Button sign up */}
+        <Link href="/sign-up">
+          <Button>
+            Register
+          </Button>
+        </Link>
+      </div>
     </div>
   )
 }
