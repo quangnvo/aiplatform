10394a9abe3ce110ff4a0693f0c0cb9f25f22ca6
quangnvo <voquang.usth@gmail.com>
2023-11-01 10:22:59 +0800

    add dashboard

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,19 +2,14 @@ import React from 'react'
 import { Button } from './ui/button'
 import { Menu } from 'lucide-react'
 import { UserButton } from '@clerk/nextjs'
+import MobileSidebar from './MobileSidebar'
 
 const Navbar = () => {
 	return (
 		<div className='flex items-center p-4'>
 
 			{/* Button toogle when in mobile */}
-			<Button
-				variant="ghost"
-				size="icon"
-				className='md:hidden'
-			>
-				<Menu />
-			</Button>
+			<MobileSidebar />
 
 			{/* Avatar */}
 			<div className='flex w-full justify-end'>
